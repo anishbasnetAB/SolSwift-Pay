defbe3b5e67a418d07b33e3f0da183991aaaa78e
anishbasnet_ab <anishbasnet027@gmail.com>
2023-03-23 15:34:04 +0545

    solving transaction logic

--- src/solona/wallet.jsx
+++ src/solona/wallet.jsx
@@ -1,46 +1,37 @@
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import { clusterApiUrl } from '@solana/web3.js';
-import React, { useMemo } from 'react';
-
+import React, { useState, useMemo } from 'react';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { GlowWalletAdapter, LedgerWalletAdapter, SolletExtensionWalletAdapter, SolletWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets';
 
-function ConectWallet() {
-  return (
-    <Context>
-      <ConnectPhantom />
-    </Context>
-  );
-}
-
-function Context({ children }) {
+function ConnectWallet({ onConnect }) {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
   const network = WalletAdapterNetwork.Devnet;
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(() => [new PhantomWalletAdapter(), new LedgerWalletAdapter(), new GlowWalletAdapter(), new SolletExtensionWalletAdapter(), new SolletWalletAdapter(), new TorusWalletAdapter()], [network]);
-
+  const wallets = useMemo(() => [new GlowWalletAdapter(), new SolletExtensionWalletAdapter(), new SolletWalletAdapter(), new LedgerWalletAdapter(), new TorusWalletAdapter()], [network]);
+  
+  const handleConnect = (wallet) => {
+    if (wallet) {
+      console.log(wallet);
+      onConnect(wallet);
+    }
+  };
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>{children}</WalletModalProvider>
+      <WalletProvider wallets={wallets} autoConnect={false}>
+        <WalletModalProvider>
+          <WalletMultiButton onConnect={handleConnect} />
+        </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
+    
   );
 }
 
-function ConnectPhantom() {
-  return (
-    <div className="App">
-      <WalletMultiButton />
-    </div>
-  );
-}
-
-export default ConectWallet;
+export default ConnectWallet;
